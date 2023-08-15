dea61b515a8fdce30213501fa1973e98bb664832
sherzodm8441 <mirsharipovsherzod@gmail.com>
2023-08-15 13:15:23 -0400

    make login/logut in navbar functional; clear user data on logout

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,17 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 function NavBar(){
+    const navigate = useNavigate();
+
+    function logOut(){
+        if(localStorage.getItem('user')){
+            localStorage.removeItem('user');
+        }
+        navigate("/");
+        window.location.reload();
+    }
+
     return (
         <div className="navbar">
-            <h3>MealIt</h3>
+            <h3 style={{color: 'lightcyan'}}>MealIt</h3>
             <div className="menu">
                 <ul>
-                    <li><Link className="navLink">Favorites</Link></li>
-                    <li><Link className="navLink">Searches</Link></li>
-                    <li><Link className="navLink">Account</Link></li>
-                    <li><Link className="navLink" key={'login'}to={'/login'}>Login</Link></li>
-                    <li><Link className="navLink" onClick={()=> console.log('logout')}>Logout</Link></li>
+                    {/* <li><Link className="navLink">Favorites</Link></li> */}
+                    {/* <li><Link className="navLink">Searches</Link></li> */}
+                    {/* <li><Link className="navLink">Account</Link></li> */}
+                    {(localStorage.getItem('user') == null) ?<li><Link className="navLink" key={'login'}to={'/login'}>Login</Link></li>
+                    :<li><Link className="navLink" onClick={()=> logOut()}>Logout</Link></li>}
                 </ul>
             </div>
         </div>
